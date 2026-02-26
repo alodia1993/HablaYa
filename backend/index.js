8513ff1f5902b2d94f19a0ec355fe8d910f3db06
Alodia <alodiahernandez93@gmail.com>
2026-02-26 19:52:53 +0100

    fix: pequeña corrección

--- backend/index.js
+++ backend/index.js
@@ -27,5 +27,5 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(` Servidor corriendo en http://localhost:${PORT}`);
 });
