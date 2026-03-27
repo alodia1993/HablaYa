99701366960e079017e80d149ef71aac70543695
Alodia <alodiahernandez93@gmail.com>
2026-03-27 12:51:51 +0100

    fix: actualizar URL de Vercel en CORS

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://hablaya.vercel.app',
+        'https://habla-ya-gamma.vercel.app',
         /\.vercel\.app$/
     ],
     credentials: true
